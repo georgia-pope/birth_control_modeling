e431a9b18bac80e4e1c8b368fdab72ac8ce21570
math189covid19 <math189covid19@gmail.com>
2020-11-06 09:10:51 -0800

    First commit

---
+++ config.py
@@ -0,0 +1,58 @@
+from collections import namedtuple
+
+# Class with all the model parameters
+class Params:
+    def __init__(self):
+        self.V_0LH = 500
+        self.V_1LH = 4500
+        self.K_mLH = 175
+        self.K_iLHP = 12.2
+        self.k_LH = 2.42
+        self.c_LHP = 0.26
+        self.c_LHE = 0.004
+        self.a_LH = 14
+        self.v_FSH = 375
+        self.tau = 1.5
+        self.K_iFSH_InhA = 1.75
+        self.k_FSH = 1.9
+        self.c_FSHP = 12
+        self.c_FSHE = 0.0018
+        self.a_FSHE = 8.21
+        self.v = 2.5
+        self.b = 0.34
+        self.K_iRcFP = 1
+        self.xi = 2.2
+        self.c_1 = 0.25
+        self.c_2 = 0.07
+        self.c_3 = 0.027
+        self.c_4 = 0.51
+        self.d_1 = 0.5
+        self.d_2 = 0.56
+        self.k_1 = 0.69
+        self.k_2 = 0.86
+        self.k_3 = 0.85
+        self.k_4 = 0.85
+        self.alpha = 0.79
+        self.gamma = 0.02
+        self.e_0 = 30
+        self.e_1 = 0.3
+        self.e_2 = 0.8
+        self.e_3 = 1.67
+        self.p_0 = 0.8
+        self.p_1 = 0.15
+        self.p_2 = 0.13
+        self.K_mPapp = 75
+        self.mu = 8 
+        self.h_0 = 0.4
+        self.h_1 = 0.009
+        self.h_2 = 0.029
+        self.h_3 = 0.018
+        self.p_dose = 0 # 0 or 0.6 or 1.3
+        self.e_dose = 0 # 0 or 40 or 92
+
+num_samples=20000
+upper_bound = 180
+initial_conditions = [29.65,6.86, 8.47, 6.15, 3.83, 11.51, 5.48, 19.27, 45.64, 100.73, 125.95, 135.84, 168.71]
+on_off = False
+variables = {'RP_LH':0, 'LH':1,' RP_FSH':2, 'FSH':3, 'RcF':4, 'GrF':5, 'DomF':6, 'Sc_1':7,'Sc_2':8, 'Lut_1':9, 'Lut_2':10, 'Lut_3':11, 'Lut_4':12}
+
---
+++ model.py
@@ -0,0 +1,114 @@
+import numpy as np
+import scipy.integrate as integrate
+from ddeint import ddeint
+import matplotlib.pyplot as plt
+
+import config as c 
+
+def calc_e_dose(t, e_dose, on_off = c.on_off, off_set=0):
+    t = t+off_set
+    if on_off:
+        if isinstance(t, np.ndarray):
+            e_dose = np.where((t%28) > 21, 0, e_dose)
+        else:
+            if (t%28) > 21:
+                e_dose=0
+    return e_dose
+
+def calc_p_dose(t, p_dose, on_off = c.on_off, off_set=0):
+    t = t+off_set
+    if on_off:
+        if isinstance(t, np.ndarray):
+            p_dose = np.where((t%28) > 21, 0, p_dose)
+        else:
+            if (t%28) > 21:
+                p_dose=0
+    return p_dose
+
+def derivs(state,t,p):
+    RP_LH, LH, RP_FSH, FSH, RcF, GrF, DomF, Sc_1, Sc_2, Lut_1, Lut_2, Lut_3, Lut_4 = state(t)
+    RP_LHd, LHd, RP_FSHd, FSHd, RcFd, GrFd, DomFd, Sc_1d, Sc_2d, Lut_1d, Lut_2d, Lut_3d, Lut_4d = state(t-p.tau)
+    e_dose = calc_e_dose(t, p.e_dose, c.on_off)
+    p_dose = calc_p_dose(t, p.p_dose, c.on_off)
+    E_2 = p.e_0 + p.e_1*GrF + p.e_2*DomF + p.e_3*Lut_4 + e_dose
+    P_4 = p.p_0 + p.p_1*Lut_3 + p.p_2*Lut_4 + p_dose
+    P_app = (P_4/2)*(1 + (E_2**p.mu / (p.K_mPapp**p.mu + E_2**p.mu)))
+    InhA = p.h_0 + p.h_1*DomF + p.h_2*Lut_2 + p.h_3*Lut_3
+    InhAd = p.h_0 + p.h_1*DomFd + p.h_2*Lut_2d + p.h_3*Lut_3d
+    
+    # Pituitary and hypothalamus axis
+    synth_LH = (p.V_0LH + (p.V_1LH*(E_2**8)/(p.K_mLH**8 + E_2**8)))/(1+(P_app/p.K_iLHP))
+    rel_LH = (p.k_LH*(1 + p.c_LHP*P_app)*RP_LH)/(1+p.c_LHE*E_2)
+    delta_RP_LH = synth_LH - rel_LH
+    
+    delta_LH = (p.k_LH*(1+p.c_LHP*P_app)*RP_LH)/(p.v*(1+p.c_LHE*E_2)) - p.a_LH*LH
+    
+    synth_FSH = p.v_FSH/(1+InhAd/p.K_iFSH_InhA)
+    rel_FSH = (p.k_FSH*(1+p.c_FSHP*P_app)*RP_FSH)/(1+p.c_FSHE*E_2**2)
+    delta_RP_FSH = synth_FSH - rel_FSH
+    
+    delta_FSH = (p.k_FSH*(1+p.c_FSHP*P_app)*RP_FSH)/(p.v*(1+p.c_FSHE*E_2**2)) - p.a_FSHE*FSH
+    
+    # Ovarian axis
+    delta_RcF = (p.b+p.c_1*RcF)*FSH/((1+(P_app/p.K_iRcFP))**p.xi) - p.c_2 * (LH**p.alpha) * RcF
+    delta_GrF = p.c_2*(LH**p.alpha)*RcF - p.c_3*LH*GrF
+    delta_DomF = p.c_3*LH*GrF - p.c_4*(LH**p.gamma)*DomF
+    delta_Sc_1 = p.c_4*LH**p.gamma*DomF - p.d_1*Sc_1
+    delta_Sc_2 = p.d_1*Sc_1 - p.d_2*Sc_2
+    delta_Lut_1 = p.d_2*Sc_2 - p.k_1*Lut_1
+    delta_Lut_2 = p.k_1*Lut_1 - p.k_2*Lut_2
+    delta_Lut_3 = p.k_2*Lut_2 - p.k_3*Lut_3
+    delta_Lut_4 = p.k_3*Lut_3 - p.k_4*Lut_4
+    
+    
+    return [delta_RP_LH, delta_LH, delta_RP_FSH, delta_FSH, delta_RcF, delta_GrF, delta_DomF, delta_Sc_1, delta_Sc_2, delta_Lut_1, delta_Lut_2, delta_Lut_3, delta_Lut_4]
+
+def initial_conditions(t):
+    return c.initial_conditions
+
+def solve():
+    params = c.Params()
+    tt = np.linspace(0, c.upper_bound, c.num_samples)
+    yy = ddeint(derivs, initial_conditions, tt, fargs=(params,))
+    return tt, yy 
+
+def calculate_E2(p, yy, tt):
+    E2 = p.e_0 + p.e_1*yy[:,c.variables['GrF']] + p.e_2*yy[:,c.variables['DomF']] + p.e_3*yy[:,c.variables['Lut_4']] + p.calc_e_dose(tt, p.e_dose)
+    return E2
+
+def calculate_P4(p, yy, tt):
+    P_4 = p.p_0 + p.p_1*yy[:,c.variables['Lut_3']] + p.p_2*yy[:,c.variables['Lut_4']] + calc_p_dose(tt, p.p_dose)
+    return P_4
+
+def get_variable(var_name, yy, tt, p):
+    if var_name == 'E_2':
+        yy = calculate_E2(p, yy, tt)
+    elif var_name == 'P_4':
+        yy = calculate_P4(p,yy,tt)
+    elif var_name in list(c.variables.keys()):
+        var_index = c.variables[var_name]
+        yy = yy[:, var_index]
+    else:
+        print('Invalid variable name')
+    return yy
+
+def plot_solution(yy, var_name, title, ylabel, tt, ylim,  num_samples = c.num_samples, on_off = c.on_off):
+    tt_mask = np.where(tt >= 84, True, False)
+    tt = tt[tt_mask]
+    yy = yy[tt_mask]
+    yy = get_variable(var_name, yy, tt, c.Params)
+    if on_off:
+        pass
+    else:
+        plt.plot(tt,yy)
+        plt.title(title)
+        plt.ylabel(ylabel)
+        plt.xlabel('time (days)')
+        plt.ylim((0,ylim))
+        plt.show()
+    
+
+tt, yy = solve()
+plot_solution(yy, 'LH', 'LH', 'LH (UI/L)', tt, 150)
+
+
